43c4ccfde31738d75396c43ee927be39b6c7ac61
Anupam Mishra <anupmis20@gmail.com>
2022-09-12 11:34:50 +0530

    All Test Case Passed Bad Horse Problem

--- 2013/Practice Round 2013/Bad Horse/Solution.cpp
+++ 2013/Practice Round 2013/Bad Horse/Solution.cpp
@@ -1,46 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(vector<pair<string, string> > names, int ind, set<string> G1, set<string> G2) {
-    for(int i = ind; i < names.size(); i++) {
-        if((find(G1.begin(), G1.end(), names[i].first) != G1.end()) && (find(G1.begin(), G1.end(), names[i].second) != G1.end())) return false;
-        else if((find(G2.begin(), G2.end(), names[i].first) != G2.end()) && (find(G2.begin(), G2.end(), names[i].second) != G2.end())) return false;
-        else if((find(G1.begin(), G1.end(), names[i].first) != G1.end()) && (find(G2.begin(), G2.end(), names[i].second) != G2.end())) continue;
-        else if((find(G1.begin(), G1.end(), names[i].second) != G1.end()) && (find(G2.begin(), G2.end(), names[i].first) != G2.end())) continue;
-        else if(find(G1.begin(), G1.end(), names[i].first) != G1.end()) G2.insert(names[i].second);
-        else if(find(G1.begin(), G1.end(), names[i].second) != G1.end()) G2.insert(names[i].first);
-        else if(find(G2.begin(), G2.end(), names[i].first) != G2.end()) G1.insert(names[i].second);
-        else if(find(G2.begin(), G2.end(), names[i].second) != G2.end()) G1.insert(names[i].first);
-        else {
-            set<string> G3 = G1;
-            set<string> G4 = G2;
-            G1.insert(names[i].first);
-            G2.insert(names[i].second);
-            G3.insert(names[i].second);
-            G4.insert(names[i].first);
-            
-            return (check(names, i + 1, G1, G2) || check(names, i + 1, G3, G4));
-        }
+string dfs(int i, vector<vector<int> > &adj, vector<bool> &V, vector<int> &color) {
+    V[i] = true;
+
+    for(int j = 0; j < adj[i].size(); j++) {
+        if(V[adj[i][j]] == false) {
+            color[adj[i][j]] = 1 - color[i];
+            string s =dfs(adj[i][j], adj, V, color);
+            if(s == "No") return s;
+        } else if(color[i] == color[adj[i][j]]) return "No";
     }
-    
-    return true;
+
+    return "Yes";
 }
 
 int main() {
     int T, M;
     string S, y;
     cin >> T;
-    
+
     for(int x = 1; x <= T; x++) {
         cin >> M;
         y = "Yes";
-        
+
         vector<pair<string, string> > names(M);
-    
-        for(int i = 0; i < M; i++) cin >> names[i].first >> names[i].second;
-        sort(names.begin(), names.end());
-        
-        if(check(names, 0, set<string>(), set<string>()) == false) y = "No";
+        unordered_map<string, int> mp;
+        int K = 1;
+
+        for(int i = 0; i < M; i++) {
+            cin >> names[i].first >> names[i].second;
+            if(mp[names[i].first] == 0) mp[names[i].first] = K++;
+            if(mp.find(names[i].second) == mp.end()) mp[names[i].second] = K++;
+        }
+
+        vector<vector<int> > adj(K);
+
+        for(int i = 0; i < M; i++) {
+            adj[mp[names[i].first]].push_back(mp[names[i].second]);
+            adj[mp[names[i].second]].push_back(mp[names[i].first]);
+        }
+
+        vector<bool> V(K, false);
+        vector<int> color(K, -1);
+        for(int i = 1; i < K; i++) {
+            if(!V[i]) {
+                color[i] = 0;
+                y = dfs(i, adj, V, color);
+                if(y == "No") break;
+            }
+        }
 
         cout << "Case #" << x << ": " << y << endl;
     }
