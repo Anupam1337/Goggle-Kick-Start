cd35d03b712f0e1ffa8cdba826503e5bb2356945
Anupam Mishra <anupmis20@gmail.com>
2022-09-25 02:25:08 +0530

    2013 B Sudoku Solver Python solution

--- 2013/Round B 2013/Sudoku Checker/Solution.py
+++ 2013/Round B 2013/Sudoku Checker/Solution.py
@@ -1,31 +1,40 @@
-from itertools import product
-
 T = int(input())
 for x in range(1, T + 1):
     N = int(input())
-    matrix = []
-    uniques = set(range(1, N ** 2 + 1))
-    valid = True
-    for row_index in range(N ** 2):
-        row = list(map(int, input().split()))
-        matrix.append(row)
-        if valid and set(row) != uniques:
-            valid = False
-    if valid:
-        for column in zip(*matrix):
-            if set(column) != uniques:
-                valid = False
+    sudoku = []
+    values = set(range(1, N ** 2 + 1))
+    y = "Yes"
+    flag = True
+    for i in range(0, N ** 2):
+        sudoku.append(list(map(int, input().split())))
+        if flag:
+            if set(sudoku[i]) != values:
+                flag = False
+                y = "No"
+    
+    if flag:
+        for i in range(0, N ** 2):
+            col = []
+            for j in range(0, N ** 2):
+                col.append(sudoku[j][i])
+            if set(col) != values:
+                flag = False
+                y = "No"
                 break
-    if valid:
-        for row_index, column_index in product(range(N), range(N)):
-            submatrix_numbers = set()
-            for row in matrix[row_index * N:(row_index + 1) * N]:
-                for number in row[column_index * N:(column_index + 1) * N]:
-                    submatrix_numbers.add(number)
-            if submatrix_numbers != uniques:
-                valid = False
+    
+    if flag:
+        for i in range(0, N):
+            for j in range(0, N):
+                block = []
+                for k in range(0, N):
+                    for l in range(0, N):
+                        block.append(sudoku[(i * N) + k][(j * N) + l])
+                
+                if set(block) != values:
+                    flag = False
+                    y = "No"
+                    break
+            if flag == False:
                 break
-    if valid:
-        print(f"Case #{x}: Yes")
-    else:
-        print(f"Case #{x}: No")
+    
+    print(f"Case #{x}: {y}")
