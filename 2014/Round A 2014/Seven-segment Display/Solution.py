12cb62be4c958e3f2f5ca91084f93fb9eabadddd
Anupam Mishra <anupmis20@gmail.com>
2022-10-03 19:51:27 +0530

    A-14 7-Bit Python Solution

--- 2014/Round A 2014/Seven-segment Display/Solution.py
+++ 2014/Round A 2014/Seven-segment Display/Solution.py
@@ -1,50 +1,45 @@
-numbers = {0: [True, True, True, True, True, True, False],
-           1: [False, True, True, False, False, False, False],
-           2: [True, True, False, True, True, False, True],
-           3: [True, True, True, True, False, False, True],
-           4: [False, True, True, False, False, True, True],
-           5: [True, False, True, True, False, True, True],
-           6: [True, False, True, True, True, True, True],
-           7: [True, True, True, False, False, False, False],
-           8: [True, True, True, True, True, True, True],
-           9: [True, True, True, True, False, True, True]}
+digits = ["1111110", "0110000", "1101101", "1111001", "0110011", "1011011", "1011111", "1110000", "1111111", "1111011"]
 
 T = int(input())
 for x in range(1, T + 1):
     states = input().split()
     N = int(states.pop(0))
-    possible = {number: [None] * 7 for number in range(10)}
-    for n, state in enumerate(states):
-        for number, segments in possible.copy().items():
-            current_possible = (number - n) % 10
-            for segment, value in enumerate(map(int, state)):
-                if value:
-                    if (not numbers[current_possible][segment]
-                            or segments[segment] == False):
-                        del possible[number]
-                        break
-                    else:
-                        possible[number][segment] = True
-                elif numbers[current_possible][segment]:
-                    if segments[segment]:
-                        del possible[number]
-                        break
-                    else:
-                        possible[number][segment] = False
     y = ""
-    for number, segments in possible.items():
-        number = (number - N) % 10
-        state = ""
-        for segment in range(7):
-            if numbers[number][segment] and segments[segment] is None:
-                state = "ERROR!"
+    mp = {n: "2222222" for n in range(10)}
+
+    for i in range(N):
+        mpr = {}
+        for disp, led in mp.items():
+            flag = 1
+
+            for k in range(7):
+                if(states[i][k] == '1'):
+                    if(digits[disp][k] == '0') or (led[k] == '0'):
+                        flag = 0
+                        break
+                    led = led[:k] + '1' + led[k + 1:]
+                elif digits[disp][k] == '1':
+                    if led[k] == '1':
+                        flag = 0
+                        break
+                    led = led[:k] + '0' + led[k + 1:]
+            if flag == 1:
+                mpr[(disp + 9) % 10] = led
+        mp = mpr.copy()
+    for disp, led in mp.items():
+        ans = digits[disp]
+        
+        for k in range(7):
+            if (ans[k] == '1') and (led[k] == '2'):
+                ans = "ERROR!"
                 break
-            state += str(int(numbers[number][segment] and segments[segment]))
-        if not y:
-            y = state
-        if state == "ERROR!" or y != state:
+            if led[k] == '0':
+                ans = ans[:k] + '0' + ans[k + 1:]
+        if y == "":
+            y = ans
+        if y != ans:
             y = "ERROR!"
-            break
-    if not y:
+    
+    if y == "":
         y = "ERROR!"
     print(f"Case #{x}: {y}")
